7c34a0385309976f2aabb1bcd1021a405acac6da
ismaldonado <isanchezm217@gmail.com>
2022-02-14 13:47:35 +0100

    implementaci4;95Rn para guardar la imagen en el servidor

--- src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.ecommerce.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ecommerce.ecommerce.model.Product;
 import com.ecommerce.ecommerce.model.User;
 import com.ecommerce.ecommerce.service.ProductService;
+import com.ecommerce.ecommerce.service.UploadFileService;
 
 @Controller
 @RequestMapping("/products")
@@ -23,6 +27,7 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+	private UploadFileService upload;
 
 	@GetMapping("")
 	public String show(Model model) { // model es un objeto que lleva info desde el backend hasta la vista
@@ -37,9 +42,21 @@ public class ProductController {
 	}
 
 	@PostMapping("/save")
-	public String save(Product product) {
+	public String save(Product product, @RequestParam("img") MultipartFile file) throws IOException {
 		User u = new User(1, "", "", "", "", "", "", "", null, null);
 		product.setUser(u);
+
+		// subir imagen
+		if (product.getId() == null) { // si se crea producto por 1ª vez
+			String imgName = upload.saveImg(file);
+			product.setPicture(imgName);
+		} else {
+			if (file.isEmpty()) { // cuando editamos el producto pero no cambiamos la imagen
+				Product p = new Product(); // creamos el producto nuevo que se vaa guardar
+				p = this.productService.get(product.getId()).get(); // se obtiene la imagen del producto anterior
+				product.setPicture(p.getPicture()); // se le asigna al nuevo objeto
+			}
+		}
 		this.productService.save(product);
 		return "redirect:/products";
 	}
--- src/main/java/com/ecommerce/ecommerce/service/UploadFileService.java
+++ src/main/java/com/ecommerce/ecommerce/service/UploadFileService.java
@@ -1,5 +1,36 @@
 package com.ecommerce.ecommerce.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
 public class UploadFileService {
+	// nombre de la carpeta
+	private String folder = "images//";
+
+	public String saveImg(MultipartFile file) throws IOException {
+		if (!file.isEmpty()) {
+			// transformo el archivo en bytes
+			byte[] bytes = file.getBytes();
+			// se guarda toda la ruta
+			Path path = Paths.get(folder + file.getOriginalFilename());
+
+			Files.write(path, bytes);
+			return file.getOriginalFilename();
+		}
+		return "default.jpg";
+	}
+
+	public void deleteImg(String name) {
+		String url = "images//";
+		File file = new File(url + name);
+		file.delete();
+	}
 
 }
