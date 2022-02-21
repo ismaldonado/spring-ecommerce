35a5e7f0d94c36eec6cbda23055fdc70e70b4806
ismaldonado <isanchezm217@gmail.com>
2022-02-21 11:21:24 +0100

    mostrar el detalle del producto al usuario

--- src/main/java/com/ecommerce/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.ecommerce.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecommerce.ecommerce.service.ProductService;
@@ -14,9 +17,18 @@ public class HomeController {
 	@Autowired
 	private ProductService productService;
 
+	// vable para imprimer cosas. no system
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
+
 	@GetMapping()
 	public String home(Model model) {
 		model.addAttribute("products", this.productService.findAll());
 		return "user/home";
+	}
+
+	@GetMapping("producthome/{id}") // se define la terminación de la ruta y el parámetro de búsqueda del producto
+	public String productHome(@PathVariable Integer id) {
+		log.info("id producto enviado como parametro {}", id);
+		return "user/producthome"; // vista a la que redirecciona el endpoint
 	}
 }
