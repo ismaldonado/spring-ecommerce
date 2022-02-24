1f13932fb7a7164ec0f83664254e62348307353c
ismaldonado <isanchezm217@gmail.com>
2022-02-24 11:57:01 +0100

    metodo para generar el numero de orden y parsearlo

--- src/main/java/com/ecommerce/ecommerce/service/order/IOrderService.java
+++ src/main/java/com/ecommerce/ecommerce/service/order/IOrderService.java
@@ -1,8 +1,11 @@
 package com.ecommerce.ecommerce.service.order;
 
+import java.util.List;
+
 import com.ecommerce.ecommerce.model.Order;
 
 public interface IOrderService {
+	List<Order> findAll();
 
 	Order save(Order order);
 }
--- src/main/java/com/ecommerce/ecommerce/service/order/OrderServiceImpl.java
+++ src/main/java/com/ecommerce/ecommerce/service/order/OrderServiceImpl.java
@@ -1,5 +1,8 @@
 package com.ecommerce.ecommerce.service.order;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +19,34 @@ public class OrderServiceImpl implements IOrderService {
 		return ordenRepository.save(order);
 	}
 
+	@Override
+	public List<Order> findAll() {
+		return this.ordenRepository.findAll();
+	}
+
+	// en la bd está como string, hay que parsearla
+	public String generateOrderNumber() {
+		int number = 0;
+		String orderNumber = "";
+		List<Order> orders = findAll();
+		List<Integer> numbers = new ArrayList<Integer>();
+		orders.stream().forEach(o -> numbers.add(Integer.parseInt(o.getNumber())));
+
+		if (numbers.isEmpty()) {
+			number = 1;
+		} else { // recorre la lista de numeros, comprara el máximo y lo devuelve.
+			number = numbers.stream().max(Integer::compare).get();
+			number++;
+		}
+
+		if (number < 10) {
+			orderNumber = "00000" + String.valueOf(number);
+		} else if (number < 100) {
+			orderNumber = "0000" + String.valueOf(number);
+		} else if (number < 1000) {
+			orderNumber = "000" + String.valueOf(number);
+		}
+		return orderNumber;
+
+	}
 }
