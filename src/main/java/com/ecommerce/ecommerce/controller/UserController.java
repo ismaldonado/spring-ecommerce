42fcdff948043fa3f8134f3bcf7d17f67038f31f
ismaldonado <isanchezm217@gmail.com>
2022-03-02 10:05:07 +0100

    mostrar vista compras usuario

--- src/main/java/com/ecommerce/ecommerce/controller/UserController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/UserController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,5 +49,12 @@ public class UserController {
 			}
 		}
 		return "redirect:/";
+	}
+
+	@GetMapping("/purchase")
+	public String getPurchase(Model model, HttpSession session) {
+		model.addAttribute("session", session.getAttribute("userId"));
+		return "user/purchase";
+
 	}
 }
